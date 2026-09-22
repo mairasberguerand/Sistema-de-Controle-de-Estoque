52f37726b410a88077fe868035bd975613901bee
Star <jajamarquesjipa@gmail.com>
2026-09-22 12:50:36 -0400

    Add files via upload

---
+++ estoque (1).h
@@ -0,0 +1,21 @@
+#ifndef ESTOQUE_H
+#define ESTOQUE_H
+
+#define MAX_ITENS 50
+#define ESTOQUE_MINIMO 5
+#define TAXA_PADRAO 0.05
+
+typedef struct {
+    int id;
+    char categoria[20];
+    char codigo_barras[20];
+    char nome[30];
+    float preco;
+    int quantidade;
+} Produto;
+
+void exibir_menu(void);
+void listar_produtos(Produto lista[], int total);
+float calcular_total(Produto lista[], int total);
+
+#endif
