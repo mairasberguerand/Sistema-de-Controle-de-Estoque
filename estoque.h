3ff0ffc49cec0cdd5a44f93afeb9d67b36c3004c
Matheus França <matheusfrassa@gmail.com>
2026-09-21 22:19:56 -0400

    Definição de juros da taxa e float de aplicação dos juros

--- estoque.h
+++ estoque.h
@@ -3,6 +3,7 @@
 
 #define MAX_ITENS 50
 #define TAXA_PADRAO 0.05
+#define TAXA_JUROS 0.08
 
 typedef struct {
     int id;
@@ -15,5 +16,6 @@ typedef struct {
 void exibir_menu(void);
 void listar_produtos(Produto lista[], int total);
 float calcular_total(Produto lista[], int total);
+float aplicar_juros(float total);
 
 #endif
